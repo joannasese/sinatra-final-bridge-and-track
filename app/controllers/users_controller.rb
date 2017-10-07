332c1699624d303d6948706ff2057bf5248df8fd
joanna <github email address>
2017-10-07 18:49:18 -0500

    Develop login.erb and login get and post requests in controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,22 @@
 class UserController < ApplicationController
 
+  get '/login' do
+    if !logged_in
+      erb :"/users/login"
+    else
+      redirect to "/home"
+    end
+  end
+
+  post '/login' do
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect to "/home"
+    else
+      redirect to "/signup"
+    end
+  end
+
 
 end
