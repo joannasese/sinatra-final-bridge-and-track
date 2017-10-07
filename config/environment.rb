09c9403528cd009262968449a3aeedcef1d7a9f6
joanna <github email address>
2017-10-07 16:22:19 -0500

    Add app files, set up environment.rb, Gemfile and Rakefile

---
+++ config/environment.rb
@@ -0,0 +1,12 @@
+#environment.rb requires gems in Gemfile 
+ENV['SINATRA_ENV'] ||= "development"
+
+require 'bundler/setup'
+Bundler.require(:default, ENV['SINATRA_ENV'])
+
+ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3",
+  :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
+)
+
+require_all 'app'
