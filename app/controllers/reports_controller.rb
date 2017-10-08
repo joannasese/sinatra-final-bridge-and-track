0e2840c1cfaa66572b0af58092919ae65e08421a
joanna <github email address>
2017-10-07 20:38:52 -0500

    Build users erb. Build reports erb.

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,3 +1,11 @@
 class ReportController < ApplicationController
+  post "/report_train" do
+    if logged_in?
+      erb :"/report/report_train"
+    else
+      redirect to '/login'
+    end
+
+  end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UserController < ApplicationController
 
   get '/login' do
-    if !logged_in
+    if !logged_in?
       erb :"/users/login"
     else
       redirect to "/home"
@@ -16,7 +16,35 @@ class UserController < ApplicationController
     else
       redirect to "/signup"
     end
+    erb :"index"
   end
 
+  get '/signup' do
+    if !logged_in?
+      erb :"/users/signup"
+    else
+      redirect to "/home"
+    end
+  end
+
+  post '/signup' do
+    if params[:username] == "" || params[:email] == "" || params[:password] == ""
+      redirect to "/signup"
+    else
+      @user = User.create(:username => params[:username], :email => params[:email], :password => params[:password])
+      @user.save
+      session[:user_id] = @user.id
+      redirect to "/home"
+    end
+  end
+
+  get '/home' do
+    erb :'users/home'
+  end
+
+  get '/logout' do
+    session.destroy
+    redirect to "/login"
+  end
 
 end
