1d0d68827c4ef51fce9e7ade10a13407fb040d67
joanna <github email address>
2017-10-09 15:50:17 -0500

    Correct 'user says'

--- app/models/bridge_report.rb
+++ app/models/bridge_report.rb
@@ -1,4 +1,5 @@
 class BridgeReport < ActiveRecord::Base
   has_many :user_reports
   has_many :users, through: :user_bridge_reports
+  has_many :comments
 end
--- app/models/train_report.rb
+++ app/models/train_report.rb
@@ -1,5 +1,5 @@
 class TrainReport < ActiveRecord::Base
   has_many :user_reports
   has_many :users, through: :user_train_reports
-  
+  has_many :comments
 end
