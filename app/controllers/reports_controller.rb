2f3bf873e6ed7967bee819c402b6b5efbbe9df37
joanna <github email address>
2017-10-09 12:12:54 -0500

    Revise get and post routes for bridge report and comment

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -7,12 +7,18 @@ class ReportController < ApplicationController
 
   post "/report_bridge" do
     if logged_in?
+
       @bridge_report = current_user.bridge_reports.create(time: params[:time])
       @bridge_report.user_id = current_user.id
-      erb :"/reports/report_bridge"
+      redirect to '/bridge_comment'
     else
       redirect to '/login'
     end
+  end
+
+  get '/bridge_comment' do
+    @bridge_report_time = current_user.bridge_reports.last.time
+    erb :"/reports/report_bridge"
   end
 
   post "/report_train" do
