ee4ed8bcfb3a2127518a99f0a9921803c6622a6a
joanna <github email address>
2017-10-10 23:44:27 -0500

    Delete comments folder in views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,6 @@ class UserController < ApplicationController
   get '/login' do
     if !logged_in?
       erb :"/users/login"
-      flash[:message] = "Please create a valid username and/or password."
     else
       redirect to "/home"
     end
