13a0706bbeaa4bde81b244f3b62b4cd426cf4622
Joanna <jmgsese@gmail.com>
2017-10-23 14:26:32 -0500

    Correct get request for bridge comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,19 +1,13 @@
 class CommentController < ApplicationController
 
   get "/bridge_comment/:id" do #visible route
-    @bridge_report = BridgeReport.find_by_id(params[:id])
-    @bridge_report_time = current_user.bridge_reports.last.time
+    @report = Report.find_by_id(params[:id])
+    # @bridge_report_time = current_user.bridge_reports.last.time
     erb :"/reports/report_bridge"
   end
 
   post "/home/bridge_comment/:id" do #invisible route
-    if params[:content] != "" #make as helper method? since code is repeated starting line 26
-      @comment = current_user.comments.create(content: params[:content])
-      @comment.user_id = current_user.id
-      @comment.bridge_report_id = current_user.bridge_reports.last.id
-      @comment.save
-      redirect to "/home" #redirects to homepage after comment page
-    end
+    post_comment
   end
 
   get '/train_comment/:id' do #visible route
