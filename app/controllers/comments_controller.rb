8011cc7cbf2a44e74e5e7263c3794d0f680eb170
Joanna <jmgsese@gmail.com>
2017-10-23 15:08:18 -0500

    Update train comment get and post requests

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,21 +2,19 @@ class CommentController < ApplicationController
 
   get "/bridge_comment/:id" do #visible route
     @report = Report.find_by_id(params[:id])
-    # @bridge_report_time = current_user.bridge_reports.last.time
     erb :"/reports/report_bridge"
   end
 
-  post "/home/bridge_comment/:id" do #invisible route
+  post "/bridge_comment/:id" do #invisible route
     post_comment
   end
 
   get '/train_comment/:id' do #visible route
     @report = Report.find_by_id(params[:id])
-    # @report_time = current_user.reports.last.time
     erb :"/reports/report_train"
   end
 
-  post '/home/train_comment/:id' do #invisible route
+  post '/train_comment/:id' do #invisible route
     post_comment
   end
 
