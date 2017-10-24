b1dcec01b48783c260e068b1ed25c0e6d61fca14
Joanna <jmgsese@gmail.com>
2017-10-24 18:38:37 -0500

    Erase commented out code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,44 +23,4 @@ class ApplicationController < Sinatra::Base
     !!current_user
   end
 
-  # def post_report(mode)
-  #   if logged_in?
-  #     @report = current_user.reports.create(time: params[:time]) #will not work with build/new
-  #     @report.user_id = current_user.id
-  #     @report.mode = mode
-  #     if @report.save
-  #       redirect to "/#{mode}_comment/#{@report.id}" #visible route
-  #     else
-  #       redirect to '/'
-  #     end
-  #   else
-  #     redirect to '/login'
-  #   end
-  # end
-
-  # def post_comment
-  #   if params[:content] != ""
-  #     @comment = current_user.comments.create(content: params[:content])
-  #     @comment.user_id = current_user.id
-  #     @comment.report_id = current_user.reports.last.id
-  #     @comment.save
-  #     redirect to "/home" #redirects to homepage after comment page
-  #   end
-  # end
-
-  # def delete_report
-    # deleting report does not appeat to automatically delete comment. comment currently deleted separately.
-    # if logged_in?
-    #   @report = Report.find_by_id(params[:id])
-    #   @comment = @report.comments.find_by(:report_id => params[:id])
-    #   @report.delete
-    #   if @comment
-    #     @comment.delete
-    #   end
-    #   redirect to "/home"
-    # else
-    #   redirect to "/login"
-    # end
-  # end
-
 end
