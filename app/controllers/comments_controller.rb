4acda0bac4843e643d0833eb44437b1bef83e186
Joanna <jmgsese@gmail.com>
2017-10-24 18:36:29 -0500

    Refine form and layout

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,12 @@
 class CommentController < ApplicationController
 
   get "/reports/:id/comments/new" do #visible route
-    @report = Report.find_by_id(params[:id])
-    erb :"/comments/new"
+    if logged_in?
+      @report = Report.find_by_id(params[:id])
+      erb :"/comments/new"
+    else
+      redirect to '/login'
+    end
   end
 
   post "/reports/:id/comments" do #invisible route
