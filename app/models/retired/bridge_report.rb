e5250874f28be6031a4624427bbd82f7ca3048fb
Joanna <jmgsese@gmail.com>
2017-10-24 17:20:37 -0500

    Delete retired models

--- app/models/retired/bridge_report.rb
+++
@@ -1,7 +0,0 @@
-class BridgeReport < ActiveRecord::Base
-  has_many :user_reports
-  has_many :users, through: :user_bridge_reports
-  has_many :comments, dependent: :destroy
-
-  validates :time, presence: true
-end
--- app/models/retired/train_report.rb
+++
@@ -1,5 +0,0 @@
-class TrainReport < ActiveRecord::Base
-  has_many :user_reports
-  has_many :users, through: :user_train_reports
-  has_many :comments, dependent: :destroy
-end
