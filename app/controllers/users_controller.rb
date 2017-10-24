81baf07e4805172c8c3e1bf650fd7c29f462c585
Joanna <jmgsese@gmail.com>
2017-10-24 17:27:25 -0500

    Reverse if/else order in post login request

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,12 +10,13 @@ class UserController < ApplicationController
 
   post '/login' do
     @user = User.find_by(username: params[:username])
-    if @user && @user.authenticate(params[:password])
-      session[:user_id] = @user.id
-      redirect to "/home"
-    else
+    if !@user
       flash[:error] = "Wrong username and/or password."
       redirect to "/login"
+    else
+      @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect to "/home"
     end
     erb :"index"
   end
